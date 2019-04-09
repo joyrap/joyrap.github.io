b91c0cf1a96191cdae23ed57a71140b95b344057
xiebiao <joyrap@gmail.com>
2019-04-09 23:28:26 +0800

    update

--- deploy_hugo_to_master.sh
+++ deploy_hugo_to_master.sh
@@ -4,30 +4,29 @@ set -eux
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Build the project. 
+hugo -d docs
+
+cp CNAME docs/
+cp -rf images docs/
+
+git add .
+git commit -m "$msg"
+git push origin hugo
+
+
 git checkout master
 
 msg="rebuilding site `date`"
 git pull
 git merge hugo
 
-if [ $# -eq 1  ]
-    then msg="$1"
-fi
-
-# Build the project. 
-hugo -d docs
-
-cp CNAME docs/
-cp -rf images docs/
 rm -rf `ls|egrep -v docs`
 mv -f ./docs/* ./
 
-# Add changes to git.
-git add .
-
-# Commit changes.
-
-git commit -m "$msg"
+if [ $# -eq 1  ]
+    then msg="$1"
+fi
 
 # Push source and build repos:
 git push origin master
